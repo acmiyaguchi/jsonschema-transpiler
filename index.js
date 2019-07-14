0b8e1c6105c152e3ca01ad8b9601e15760416683
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2019-07-13 22:35:59 -0700

    Use bootstrap for grids

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const rust = import('./pkg/jst');
 
+import "bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import CodeFlask from 'codeflask';
 import JSONFormatter from 'json-formatter-js'
 
@@ -29,10 +32,11 @@ const flask = new CodeFlask('#editor', {
     lineNumbers: true,
     defaultTheme: false
 });
+flask.updateCode(JSON.stringify(schema, null, 2));
 
 rust.then(jst => {
     let transpiled = jst.convert_bigquery_js(schema);
     console.log(transpiled);
     let formatter = new JSONFormatter(transpiled);
-    document.body.appendChild(formatter.render());
+    document.getElementById("output").appendChild(formatter.render());
 }).catch(console.error);
